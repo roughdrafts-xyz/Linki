21c38a451d90d5278fd228d961d355892af4186d
Minty <kinostl@gmail.com>
2023-01-26 12:27:25 -0500

    Added some more stuff testing critical things more directly.

---
+++ sigil/tests/repo/test_generateNewRefid.py
@@ -0,0 +1,44 @@
+import unittest
+import os
+from sigil.repo.Repo import Repo
+from sigil.tests.helpers import getInitializedDirectory
+
+
+class TestGenerateNewRefid(unittest.TestCase):
+    def setUp(self):
+        self.dir = getInitializedDirectory()
+        os.chdir(self.dir.name)
+        self.repo = Repo()
+
+    def tearDown(self):
+        self.dir.cleanup()
+
+    def test_refids_different_with_different_content(self):
+        with open('hello_world.md', 'w') as file:
+            file.write('Hello World!')
+
+        with open('goodnight_moon.md', 'w') as file:
+            file.write('Goodnight Moon!')
+
+        hw_refid = self.repo._generateNewRefid('', 'hello_world.md')
+        gm_refid = self.repo._generateNewRefid('', 'goodnight_moon.md')
+        up_refid = self.repo._generateNewRefid(hw_refid, 'goodnight_moon.md')
+
+        self.assertNotEqual(hw_refid, gm_refid)
+        self.assertNotEqual(gm_refid, up_refid)
+        self.assertNotEqual(hw_refid, up_refid)
+
+    def test_refids_different_with_same_content(self):
+        with open('hello_world.md', 'w') as file:
+            file.write('Goodnight Moon!')
+
+        with open('goodnight_moon.md', 'w') as file:
+            file.write('Goodnight Moon!')
+
+        hw_refid = self.repo._generateNewRefid('', 'hello_world.md')
+        gm_refid = self.repo._generateNewRefid('', 'goodnight_moon.md')
+        up_refid = self.repo._generateNewRefid(hw_refid, 'goodnight_moon.md')
+
+        self.assertNotEqual(hw_refid, gm_refid)
+        self.assertNotEqual(gm_refid, up_refid)
+        self.assertNotEqual(hw_refid, up_refid)
