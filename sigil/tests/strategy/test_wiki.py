0d7275209648a6b0caaf64b69dd379e63b42eb65
Minty <kinostl@gmail.com>
2023-03-16 23:32:09 -0400

    Added some more complex tests for wiki

--- sigil/tests/strategy/test_wiki.py
+++ sigil/tests/strategy/test_wiki.py
@@ -26,4 +26,54 @@ class TestWiki(unittest.TestCase):
             self.remote_articles._log
         )
 
-        pass
+    def test_does_sync_with_history(self):
+        rprefid = self.remote_articles.add_article(b'Hello Moon')
+        lprefid = self.local_articles.add_article(b'Hello Sun')
+        self.remote_articles.update_article(
+            refId=rprefid,
+            content=b'Goodnight Moon'
+        )
+        self.local_articles.update_article(
+            refId=lprefid,
+            content=b'Goodnight Sun'
+        )
+
+        self.wiki.sync()
+
+        self.assertCountEqual(
+            self.local_articles._data,
+            self.remote_articles._data
+        )
+
+        self.assertCountEqual(
+            self.local_articles._log,
+            self.remote_articles._log
+        )
+
+    def test_does_sync_with_weird_history(self):
+        """
+        Weird history is defined as when two repositories incidentally create the same history through whatever means. This is meant to be usable with sneakernets, so this might happen from time to time.
+        """
+        prefid = self.remote_articles.add_article(b'Hello Moon')
+        self.local_articles.add_article(b'Hello Moon')
+        self.remote_articles.update_article(
+            refId=prefid,
+            content=b'Goodnight Moon'
+        )
+
+        self.local_articles.update_article(
+            refId=prefid,
+            content=b'Goodnight Moon'
+        )
+
+        self.wiki.sync()
+
+        self.assertCountEqual(
+            self.local_articles._data,
+            self.remote_articles._data
+        )
+
+        self.assertCountEqual(
+            self.local_articles._log,
+            self.remote_articles._log
+        )
