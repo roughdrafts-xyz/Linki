777fdba96be849cba7bbc5e472b5692a650e6102
Minty <kinostl@gmail.com>
2023-03-18 03:27:59 -0400

    Changed over to toml due to setuptools docs.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='sigil',
-    version='1.0',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[]
-)
