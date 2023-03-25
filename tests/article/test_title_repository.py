64c72f8f9e57c06147ae30fa27323b760c8f5c45
Minty <kinostl@gmail.com>
2023-03-25 09:55:29 -0400

    Made TestTitleRepository

--- tests/article/test_title_repository.py
+++ tests/article/test_title_repository.py
@@ -1,8 +1,7 @@
 from contextlib import contextmanager
 import pytest
-from sigili.article.repository import MemoryArticleRepository
 
-from sigili.title.repository import TitleDetails
+# from sigili.title.repository import TitleDetails
 
 
 # CAR needs to be able to track what the CAs are.
@@ -10,34 +9,56 @@ from sigili.title.repository import TitleDetails
 
 # Titles is the choice of phrase for current titles
 
+class TestTitleRepository():
+    def __init__(self) -> None:
+        self.titles: dict[str, TitleDetails] = dict()
+        self.store: dict[str, list[TitleDetails]] = dict()
+        pass
+
+    def set_title(self, title, articleId) -> TitleDetails:
+        _title_detail = TitleDetails(
+            articleId
+        )
+
+        if (title not in self.store):
+            self.store[title] = []
+        self.store[title].append(_title_detail)
+        self.titles[title] = _title_detail
+        return _title_detail
+
+    def get_title(self, title) -> TitleDetails:
+        return self.titles[title]
+
+    def get_titles(self) -> list[TitleDetails]:
+        return list(self.titles.values())
+
+    def get_options(self, title) -> list[TitleDetails]:
+        return list(self.store[title])
+
+
 @contextmanager
 def getTitleRepository(style: str):
     match style:
-        case MemoryTitleRepository.__name__:
-            yield MemoryTitleRepository()
         case TestTitleRepository.__name__:
             yield TestTitleRepository()
-        case FileSystemTitleRepository.__name__:
-            _dir = TemporaryDirectory()
-            _dirPath = Path(_dir.name)
-            _contentPath = FileSystemTitleRepository.initialize_directory(
-                _dirPath)
-            try:
-                yield FileSystemTitleRepository(path=_contentPath)
-            finally:
-                _dir.cleanup()
+#       case MemoryTitleRepository.__name__:
+#           yield MemoryTitleRepository()
+#       case FileSystemTitleRepository.__name__:
+#           _dir = TemporaryDirectory()
+#           _dirPath = Path(_dir.name)
+#           _contentPath = FileSystemTitleRepository.initialize_directory(
+#               _dirPath)
+#           try:
+#               yield FileSystemTitleRepository(path=_contentPath)
+#           finally:
+#               _dir.cleanup()
 
 
 styles = {
-    MemoryTitleRepository.__name__,
-    FileSystemTitleRepository.__name__,
     TestTitleRepository.__name__,
+    #   MemoryTitleRepository.__name__,
+    #   FileSystemTitleRepository.__name__,
 }
-
-
-@pytest.fixture
-def article_update():
-    return ArticleUpdate()
 
 
 @pytest.mark.parametrize('style', styles)
