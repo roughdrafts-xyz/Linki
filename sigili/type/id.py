571fd0560019ebe2f4ca23504132b2efcba7a63e
Minty <kinostl@gmail.com>
2023-03-25 20:32:40 -0400

    Made Article IDs check themselves

--- sigili/type/id.py
+++ sigili/type/id.py
@@ -1,8 +1,27 @@
+from collections import UserString
 import re
 
 
-SHA224 = re.compile("[a-f0-9]{56}")
-ArticleID = str
-DraftID = str
-TitleID = str
-SourceID = str
+SHA224 = re.compile(r"[a-f0-9]{56}")
+
+
+class _ID(UserString):
+    def __init__(self, seq: str) -> None:
+        assert SHA224.match(seq)
+        super().__init__(seq)
+
+
+class ArticleID(_ID):
+    pass
+
+
+class DraftID(_ID):
+    pass
+
+
+class TitleID(_ID):
+    pass
+
+
+class SourceID(_ID):
+    pass
