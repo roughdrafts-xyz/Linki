0de0a878c7edc324fa38c83f8f3791bdf6fdddb0
Minty <kinostl@gmail.com>
2023-04-07 21:42:45 -0400

    DraftRepo uses connection method

--- sigili/draft/repository.py
+++ sigili/draft/repository.py
@@ -1,13 +1,12 @@
 
-from abc import ABC, abstractmethod
-from dataclasses import asdict, dataclass, field
-import json
+from abc import ABC
+from dataclasses import dataclass, field
 from pathlib import Path
-import pickle
 from typing import Iterator
 from sigili.article.repository import Article, ArticleUpdate
+from sigili.connection import Connection, MemoryConnection, PathConnection
 
-from sigili.type.id import ArticleID, ContentID, Label, LabelID
+from sigili.type.id import ContentID, Label
 
 
 @dataclass
@@ -48,42 +47,8 @@ class Draft:
         )
 
 
-@dataclass
-class SparseDraft:
-    title: Label
-    groups: list[Label]
-    editOf: Article | None = None
-
-    @classmethod
-    def fromDraft(cls, draft: Draft):
-        return cls(
-            draft.title,
-            draft.groups,
-            draft.editOf
-        )
-
-
 class DraftRepository(ABC):
-    @abstractmethod
-    def set_draft(self, draft: Draft) -> Draft:
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_draft(self, title: Label) -> Draft | None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_drafts(self) -> Iterator[Draft]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def clear_draft(self, title: Label) -> bool:
-        raise NotImplementedError
-
-
-class MemoryDraftRepository(DraftRepository):
-    def __init__(self) -> None:
-        self.drafts: dict[LabelID, Draft] = dict()
+    drafts: Connection[Draft]
 
     def set_draft(self, draft: Draft) -> Draft:
         self.drafts[draft.title.labelId] = draft
@@ -102,59 +67,14 @@ class MemoryDraftRepository(DraftRepository):
         return False
 
 
+class MemoryDraftRepository(DraftRepository):
+    def __init__(self) -> None:
+        self.drafts = MemoryConnection()
+
+
 class FileSystemDraftRepository(DraftRepository):
     def __init__(self, path: Path) -> None:
-        self._path = path.resolve()
-        self._content = self._path.joinpath('content')
-        self._data = self._path.joinpath('data')
-
-    def dump_draft(self, draft: Draft, path: Path) -> SparseDraft:
-        with path.open('wb') as _path:
-            _draft = SparseDraft.fromDraft(draft)
-            pickle.dump(_draft, _path)
-            return _draft
-
-    def load_draft(self, path: Path) -> SparseDraft:
-        with path.open('rb') as _path:
-            _draft = pickle.load(_path)
-            return _draft
-
-    def set_draft(self, draft: Draft) -> Draft:
-        _content = self._content.joinpath(draft.title.name)
-        _data = self._data.joinpath(draft.title.name)
-        _content.write_bytes(draft.content)
-        self.dump_draft(draft, _data)
-        return draft
-
-    def get_draft(self, title: Label) -> Draft | None:
-        content_path = self._content.joinpath(title.name)
-        data_path = self._data.joinpath(title.name)
-        if (not (content_path.exists() and data_path.exists())):
-            return None
-        _content = content_path.read_bytes()
-        _data = self.load_draft(data_path)
-        return Draft(
-            _data.title,
-            _content,
-            _data.groups,
-            _data.editOf
-        )
-
-    def get_drafts(self) -> Iterator[Draft]:
-        for draft in self._data.iterdir():
-            title = Label(draft.name)
-            _draft = self.get_draft(title)
-            if (_draft is not None):
-                yield _draft
-
-    def clear_draft(self, title: Label) -> bool:
-        content_path = self._content.joinpath(title.name)
-        data_path = self._data.joinpath(title.name)
-        if (not (content_path.exists() and data_path.exists())):
-            return False
-        content_path.unlink()
-        data_path.unlink()
-        return True
+        self.drafts = PathConnection(path.resolve())
 
     @staticmethod
     def init(path: Path):
@@ -162,10 +82,4 @@ class FileSystemDraftRepository(DraftRepository):
             raise FileNotFoundError
         _draftPath = path.joinpath('drafts')
         _draftPath.mkdir()
-
-        _contentPath = _draftPath.joinpath('content')
-        _contentPath.mkdir()
-
-        _dataPath = _draftPath.joinpath('data')
-        _dataPath.mkdir()
         return _draftPath
