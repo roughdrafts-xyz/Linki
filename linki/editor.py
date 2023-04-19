4509b4983d4f03d5ed197bc528fc5d662334e093
Minty <kinostl@gmail.com>
2023-04-19 07:08:02 -0400

    Fixed publish issue

--- linki/editor.py
+++ linki/editor.py
@@ -29,6 +29,7 @@ class Editor():
             count += 1
             article = self.repo.articles.merge_article(draft.asArticle())
             self.repo.titles.set_title(article)
+            self.repo.drafts.clear_draft(draft.label)
         return count
 
     def copy_articles(self, articles: ArticleCollection):
