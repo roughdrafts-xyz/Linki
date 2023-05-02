4ba1c62fdcab648d950dd0b56768f9a6a01ae4bc
Minty <kinostl@gmail.com>
2023-05-02 18:10:04 -0400

    Add some help files to the commands

--- linki/main.py
+++ linki/main.py
@@ -10,9 +10,9 @@ from linki.outbox import Outbox
 from linki.repository import FileRepository, Repository
 from linki.viewer import WebView, WebViewConf
 
-
 app = typer.Typer(
-    no_args_is_help=True
+    no_args_is_help=True,
+    help="A tool to create distributed wikis, and link them together through subscriptions and contributions."
 )
 
 
@@ -21,6 +21,11 @@ def init(
     destination: Path = typer.Argument(Path.cwd()),
     silent: bool = typer.Option(False)
 ):
+    """
+    This is how you get started!
+
+    Just go ahead and run it in the folder you want to use as your linki, or give it the destination of where you want your linki.
+    """
     if (not destination.exists()):
         destination.mkdir()
     FileRepository.createPath(destination)
@@ -33,6 +38,19 @@ def publish(
     location: Path = typer.Argument(Path.cwd()),
     contribute: bool = typer.Option(False)
 ):
+    """
+    Write your drafts to the linki.
+
+    When you run this command it takes all your drafts and saves them as titles to your linki. Any overwritten titles get added to your article repository to act as history. You can send out updates to wikis you contribute to using the --contribute flag  
+
+    ### Titles ###
+
+    A title is an article that is on display. They think in paths. On the file system, this is the file's path from the root (the folder you initialized). On the web, this is the file's path in the url.  
+
+    ### Articles ###
+
+    When you publish a title, you create an article. Articles are stored seperately from titles, and they think in ids. Consider them a title's history. On the file system and on the web, this is all encapsulated inside the linki program and commands in it allow you to interact with the articles in a title's history.
+    """
     editor = FileEditor.fromPath(location)
     editor.load_drafts()
     x = editor.publish_drafts()
@@ -47,6 +65,11 @@ def publish(
 
 @app.command()
 def copy(source: str, destination: Path = typer.Argument(Path.cwd(), file_okay=False)):
+    """
+    Replicate another wiki
+
+    Do you like something you see on someone else's linki? Go ahead and run this command pointing at the linki you'd like to replicate. It'll copy over the whole linki and all of its history. Its yours to change and host now.
+    """
     if (not destination.exists()):
         init(destination, True)
     source_repo = Repository(source)
@@ -61,6 +84,11 @@ def copy(source: str, destination: Path = typer.Argument(Path.cwd(), file_okay=F
 
 @app.command()
 def subscribe(url: str, location: Path = typer.Argument(Path.cwd())):
+    """
+    Follow a wiki for updates
+
+    When you run linki inbox, it will check what you're subscribed to and if that linki has any updates. If it does have any updates, it'll download them to your inbox
+    """
     repo = FileRepository.fromPath(location)
     subs = repo.subs
     subs.add_url(url)
@@ -71,6 +99,11 @@ def subscribe(url: str, location: Path = typer.Argument(Path.cwd())):
 def subscriptions(
     location: Path = typer.Argument(Path.cwd())
 ):
+    """
+    DEPRECATED
+
+    Needs to become subscribe --list
+    """
     repo = FileRepository.fromPath(location)
     subs = repo.subs
     typer.echo(f"Subscriptions by priority (highest to lowest)")
@@ -86,6 +119,11 @@ def inbox(
     location: Path = typer.Option(Path.cwd()),
     copy_id: Optional[str] = typer.Argument(None)
 ):
+    """
+    See changes from your subscriptions and contributions
+
+    When you make a contribution, its stored in the inbox. When a subscription updates, the changes are stored in the inbox. This lets you view them all or individual changes. You can use `linki approve` or `linki refuse` to choose what you will do with these changes.
+    """
     repo = FileRepository.fromPath(location)
     inbox = Inbox(repo)
     inbox.load_inbox()
@@ -102,6 +140,11 @@ def refuse(
     copy_id: str = typer.Argument(None),
     list: bool = typer.Option(False)
 ):
+    """
+    Refuse a change
+
+    When you run this command pointing at a copy id (the ids to the left side of the inbox command), it'll save that id and ignore it when updating the inbox in the future.
+    """
     repo = FileRepository.fromPath(location)
     if (list):
         typer.echo(repo.config.render_refusals())
@@ -118,6 +161,11 @@ def approve(
     copy_id: str = typer.Argument(None),
     list: bool = typer.Option(False)
 ):
+    """
+    Accept a change
+
+    When you run this command pointing at a copy id (the ids to the left side of the inbox command), it'll copy those changes into your wiki.
+    """
     editor = FileEditor.fromPath(location)
     if (list):
         typer.echo(editor.repo.config.render_approvals())
@@ -137,9 +185,20 @@ def serve(
     contribute: bool = typer.Option(False),
     debug: bool = typer.Option(False, hidden=True),
     host: str = typer.Option('localhost'),
-    port: int = typer.Option(8080),
+    port: int = typer.Option(80),
     home: Optional[Path] = typer.Option(None),
 ):
+    """
+    Run a web server!
+
+    When you run this, you can have a read-only web interface that displays your articles on the web. You'll need to provide your own machine and url of course, but this will make the process very easy.
+
+    You can toggle the api, web views, and linki specific endpoints such as copying and contributing.
+
+    To use the web views, you must have pandoc installed. You can install it with `linki install-pandoc`
+
+    To accept contributions, you must add contributing users with `linki authenticate` and run this command with the --contribute flag.
+    """
     try:
         if (web):
             pypandoc.get_pandoc_path()
@@ -166,6 +225,9 @@ def serve(
 
 @ app.command(hidden=True)
 def install_pandoc():
+    """
+    Installs pandoc
+    """
     try:
         pandoc = pypandoc.get_pandoc_path()
         if (pandoc is not None):
@@ -182,6 +244,11 @@ def contribute(
     username: Optional[str] = typer.Option(None),
     password: Optional[str] = typer.Option(None)
 ):
+    """
+    Contribute to a linki!
+
+    When you contribute to a linki you can use `linki publish --contribute` to send your changes to linkis that you contribute to. Just add the urls for those linkis to your contributors list by using this command and point it at the root of the linki you're contributing to.
+    """
     repo = FileRepository.fromPath(location)
     new_url = repo.contribs.add_url(url)
     if (new_url.parsed.scheme == 'https'):
@@ -203,6 +270,11 @@ def contribute(
 def contributions(
     location: Path = typer.Argument(Path.cwd()),
 ):
+    """
+    DEPRECATED
+
+    This needs to become `linki contribute --list`
+    """
     repo = FileRepository.fromPath(location)
     contribs = repo.contribs
     typer.echo(f"Contributions by priority (highest to lowest)")
@@ -220,6 +292,11 @@ def authenticate(
     password: str = typer.Option(..., prompt=True,
                                  hide_input=True, confirmation_prompt=True)
 ):
+    """
+    Allow someone to contribute to your linki
+
+    This version of linki only allows username and passwords as a way to authenticate a user. Use this command to add a new user to your linki, and they'll be able to send your linki contributions when you run a server with `linki serve`.
+    """
     repo = FileRepository.fromPath(location)
     repo.users.add_user(user, password)
     typer.echo(f"Added {user} to list of authorized users.")
